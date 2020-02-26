bf415ea9ccd4acd2f72c564d2bd1f10ff828145a
skraus5628 <s_read@aol.com>
2020-02-25 20:41:03 -0600

    finished

--- README.md
+++ README.md
@@ -13,3 +13,56 @@ In this project we will cover the basics of `Structure Query Language (SQL)`, `R
 - [ ] fork and clone this repository.
 - [ ] **CD into the folder** where you cloned **your fork**.
 - [ ] type `npm install` to download dependencies.
+
+                        --This is (DQL) Data Query Language--
+
+SELECT <columns>
+FROM <table>
+WHERE <column> = <value>;
+
+<!-- Examples -->
+<!-- Select "ShippedDate"
+FROM "Order"
+WHERE "id" = 19570;
+
+SELECT * 
+FROM "Product"
+WHERE "UnitsInStock" >= 100
+ORDER BY "UnitsInStock"
+LIMIT 3;
+
+SELECT *
+FROM "Product"
+ORDER BY "UnitPrice" DESC
+LIMIT 1
+OFFSET 3; -->
+
+
+                    --This is Data Manipulation Language (DML)--
+
+<!-- EDIT -->
+INSERT INTO <Table> (<column1>, <column3>)
+VALUES (<value1>, <value3>);
+
+<!-- UPDATE -->
+UPDATE <table>
+SET <column> = <new value>
+WHERE <column> = <value>;
+
+<!-- DELETE -->
+DELETE FROM <table>
+WHERE <column> = <value>;
+
+
+<!-- Examples -->
+<!-- --INSERT INTO "Category" ("CategoryName", "Description")
+--VALUES ("Frozen", "Ready to eat Meals");
+
+
+--UPDATE "Category"
+--SET "Description" = 'Deserts and ready to eat meals'
+--WHERE "Id" =9;
+
+DELETE FROM "Category"
+WHERE "Id" =8; -->
+
--- posts/post-router.js
+++ posts/post-router.js
@@ -4,23 +4,71 @@ const db = require("../data/config")
 const router = express.Router()
 
 router.get("/", async (req, res, next) => {
-
+    try{
+         //  translates to "SELECT * FROM posts"
+        const posts = await db.select("*").from("posts")
+        res.json(posts)
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.get("/:id", async (req, res, next) => {
-
+    try{
+        //  translates to 'SELECT * FROM posts WHERE Id =? LIMIT 1;'
+        // const post = await db.first("*").from("posts").where("id", req.params.id)
+        // these do the same thing but leave room for multiple params
+        const post = await db.first("*").from("posts").where({ id: req.params.id })
+        res.json(post)
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.post("/", async (req, res, next) => {
+    try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
 
+        // Translates to 'INSERT INTO posts (title, contents) VALUES (7,7);'
+        // await db.insert(payload).into("posts")
+        // this does same thing but shorter, when using KNEX code, order doesn't matter.
+        // Gets the ID of the fresh database row
+        const [id] = await db("posts").insert(payload)
+        const newPost = await db("posts").where("id", id).first()
+        res.json(newPost);
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.put("/:id", async (req, res, next) => {
+    try{
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
 
+        // translates to 'UPDATE posts SET ? = ? Where id=?'
+        await db("posts").where("id", req.params.id).update(payload)
+        const post = await db("posts").where("id", req.params.id).first()
+
+        res.json(post)
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.delete("/:id", async (req, res, next) => {
-
+    try{
+        // translates to 'DELETE FROM posts WHERE id = '?'
+        await db("posts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
